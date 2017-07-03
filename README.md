06cfad9665ec7e2480bdb73a936a51bc46c99f1f
Abdennebi Mohamed <abdennebi@gmail.com>
2017-07-03 23:27:13 +0200

    Create server nodes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # nomad-consul-fabio-dnsmasq-gcp-demo
+
+## Prerequisites
+
+## Setup
+
+1. Create a new project in Google Compute Engine and call it : ``nomad-consul-fabio-dnsmasq``. When the project is ready, open the *Cloud Shell*.
+
+2. Create three Compute Engine instances named respectively `c1`, `c2` and `c3`. The startup script server-install.sh will install and configure **Nomad** and **Consul**.
+
+```
+gcloud compute instances create c1 c2 c3 \
+  --image-project ubuntu-os-cloud \
+  --image-family ubuntu-1604-lts \
+  --zone=us-west1-a \
+  --boot-disk-size 10GB \
+  --machine-type n1-standard-1 \
+  --can-ip-forward \
+  --metadata-from-file startup-script=server-install.sh
+```
---
+++ server-install.sh
@@ -0,0 +1,104 @@
+export IP_ADDRESS=$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+
+apt-get update
+apt-get install -y unzip dnsmasq
+
+## Setup Nomad
+
+wget https://releases.hashicorp.com/nomad/0.5.6/nomad_0.5.6_linux_amd64.zip
+unzip nomad_0.5.6_linux_amd64.zip -d /usr/local/bin/
+
+mkdir -p /var/lib/nomad
+mkdir -p /etc/nomad
+
+rm nomad_0.5.6_linux_amd64.zip
+
+cat > server.hcl <<EOF
+addresses {
+    rpc  = "ADVERTISE_ADDR"
+    serf = "ADVERTISE_ADDR"
+}
+
+advertise {
+    http = "ADVERTISE_ADDR:4646"
+    rpc  = "ADVERTISE_ADDR:4647"
+    serf = "ADVERTISE_ADDR:4648"
+}
+
+bind_addr = "0.0.0.0"
+data_dir  = "/var/lib/nomad"
+log_level = "DEBUG"
+
+server {
+    enabled = true
+    bootstrap_expect = 3
+}
+EOF
+
+sed -i "s/ADVERTISE_ADDR/${IP_ADDRESS}/" server.hcl
+mv server.hcl /etc/nomad/server.hcl
+
+cat > nomad.service <<EOF
+[Unit]
+Description=Nomad
+Documentation=https://nomadproject.io/docs/
+
+[Service]
+ExecStart=/usr/local/bin/nomad agent -config /etc/nomad
+ExecReload=/bin/kill -HUP $MAINPID
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+mv nomad.service /etc/systemd/system/nomad.service
+
+systemctl enable nomad
+systemctl start nomad
+
+## Setup Consul
+
+mkdir -p /var/lib/consul
+wget https://releases.hashicorp.com/consul/0.8.5/consul_0.8.5_linux_amd64.zip
+unzip consul_0.8.5_linux_amd64.zip -d /usr/local/bin/
+rm consul_0.6.4_linux_amd64.zip
+
+cat > consul.service <<EOF
+[Unit]
+Description=consul
+Documentation=https://consul.io/docs/
+
+[Service]
+ExecStart=/usr/local/bin/consul agent \
+  -advertise=ADVERTISE_ADDR \
+  -bind=0.0.0.0 \
+  -bootstrap-expect=3 \
+  -client=0.0.0.0 \
+  -data-dir=/var/lib/consul \
+  -server \
+  -ui
+  
+ExecReload=/bin/kill -HUP $MAINPID
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sed -i "s/ADVERTISE_ADDR/${IP_ADDRESS}/" consul.service
+
+mv consul.service /etc/systemd/system/consul.service
+systemctl enable consul
+systemctl start consul
+
+## Setup dnsmasq
+
+mkdir -p /etc/dnsmasq.d
+cat > /etc/dnsmasq.d/10-consul <<EOF
+server=/consul/127.0.0.1#8600
+EOF
+
+systemctl enable dnsmasq
+systemctl start dnsmasq
