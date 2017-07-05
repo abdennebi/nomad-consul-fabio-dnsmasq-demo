09f7d9a30b3e9c0564eb27b61db0d9b224edb347
Abdennebi Mohamed <abdennebi@gmail.com>
2017-07-06 00:24:55 +0200

    Add dnsmasq

--- client-install.sh
+++ client-install.sh
@@ -60,3 +60,13 @@ mv nomad.service /etc/systemd/system/nomad.service
 
 systemctl enable nomad
 systemctl start nomad
+
+## Setup dnsmasq
+
+mkdir -p /etc/dnsmasq.d
+cat > /etc/dnsmasq.d/10-consul <<'EOF'
+server=/consul/127.0.0.1#8600
+EOF
+
+systemctl enable dnsmasq
+systemctl start dnsmasq
